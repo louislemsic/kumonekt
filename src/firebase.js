fa8b754c5fc9c2b2da7795916564b0367585b1fc
James Louis Lemsic <lemsicode@gmail.com>
2021-07-19 01:05:32 +0800

    Updated firebase.js
    Sensitive information. Deleted for the public view.

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,6 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 export const auth = firebase.initializeApp({
-    apiKey: "AIzaSyAaqjfCvs4Of1eR8jYIzfcoBRg34Nm1M9U",
-    authDomain: "kumonekt.firebaseapp.com",
-    projectId: "kumonekt",
-    storageBucket: "kumonekt.appspot.com",
-    messagingSenderId: "401918226760",
-    appId: "1:401918226760:web:c1c48c427d1b0136a0437d",
-    measurementId: "G-BPM9NF44Z8"
+    //firebase app config here
+    //erased due to sensitive information
 }).auth();
